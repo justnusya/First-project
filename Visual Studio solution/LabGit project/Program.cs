4a7b9dd55a65b861298d722a5e2265a6060feb1c
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2023-11-16 21:44:04 +0200

    Додано підрахунок суми чисел і виведення результату

--- Visual Studio solution/LabGit project/Program.cs
+++ Visual Studio solution/LabGit project/Program.cs
@@ -10,7 +10,9 @@ namespace LabGit_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
             Console.ReadKey();
         }
     }
